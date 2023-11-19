ddcc58657f10f0e56becdc6bb57a4ac89797d38c
FARDIN98 <fardinahmed66@gmail.com>
2023-11-19 23:45:59 +0600

    first commit

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+    const DB_HOST = "localhost";
+    const DB_USER = "root";
+    const DB_PASS = "";
+    const DB_NAME = "EduShareHub";
+    const SITE_DOMAIN = 'http://localhost';
+    const SITE_DIR = '/EduShareHub';
+
+    require_once 'pages.php';
